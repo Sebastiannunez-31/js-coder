6da9efff91101c83e18c632932284dbc34f7932c
sebastiannunez-31 <seba.hinos@gmail.com>
2022-08-30 00:11:47 -0400

    desafio simulador - terminado

--- javascript/index.js
+++ javascript/index.js
@@ -1,34 +1,133 @@
-let operacion = prompt("Ingresa una operacion \n suma \n resta\n multiplicar\n dividir").toUpperCase();
 
 
 
-while (operacion != "ESC") {
-    let numero = Number(prompt("ingresa un numero"));
-    let numeroDos = Number(prompt("ingrese otro numero"));
-    switch (operacion) {
 
-        case "SUMA":       
-            let suma = numero + numeroDos;
-            alert(`tu resultado de la suma de los numeros ${numero} y ${numeroDos}, es  ${suma}`);
-            break;
-        case "RESTA":
-            let resta = numero - numeroDos;
-            alert(`tu resultado de la resta de los numeros ${numero} y ${numeroDos}, es  ${resta}`);
-            break;
-        case "MULTIPLICAR":
-            let multilicacion = numero * numeroDos;
-            alert(`tu resultado de la multiplicacion de los numeros ${numero} y ${numeroDos}, es  ${multilicacion}`);
-            break;
-        case "DIVIDIR":
-            let division = numero / numeroDos;
-            alert(`tu resultado de la division de los numeros ${numero} y ${numeroDos}, es  ${division}`);
-            break;
-        default:
-            alert("INGRESA UNA OPCION VALIDA");
-            break;
+function viaje(costo = Number) {
+
+
+    alert(`el total de su viajees de ${costo}`)
+    let metodoPago = parseInt(prompt("seleccione metodo de pago \n 1- credito \n 2- debito \n"));
+
+    //*metodo de pago para seleccion credito
+    if (metodoPago === 1) {
+
+        let pago = parseInt(prompt(`el total de su compra es ${costo} \n en cuantas cuotas desea pagar? \n 3 - 6 - 12`));
+
+
+        if (pago === 3) {
+
+
+            total = costo / 3
+
+            alert(`su total a pagar por ${pago} cuotas es de ${total * 1.35}`);
+
+            pagar();
+        }
+
+
+        else if (pago === 6) {
+
+            total = costo / 6
+
+            alert(`su total a pagar por ${pago} cuotas es de ${total * 1.85}`)
+            pagar();
+
+        }
+        else if (pago === 12) {
+
+            total = costo / 12
+
+            alert(`su total a pagar por ${pago} cuotas es de ${total * 1.95}`)
+            pagar();
+
+        }
+    }
+    //*metodo de pago selccion  debito
+    else if (metodoPago === 2) {
+
+        alert(" seleccione su banco para realizar su pago")
+
+        let banco = parseInt(prompt("1- banco estado \n 2- banco de chile"));
+
+        if (banco === 1) {
+
+            alert("bienvenido a banco estado")
+
+            let usuario = prompt("ingresa tu nombre de usuario")
+            alert(`bienvenido ${usuario}`)
+
+            contraseña = parseInt(prompt("ingresa tu clave de 4 digitos"));
+
+            alert("dirigiendo al pago")
+
+            pagar();
+
+
+        } else if (banco === 2) {
+            alert("bienvenido a banco de chile")
+
+            let usuario = prompt("ingresa tu nombre de usuario")
+            alert(`bienvenido ${usuario}`)
+
+            contraseña = parseInt(prompt("ingresa tu clave de 4 digitos"));
+
+            alert("dirigiendo al pago")
+
+
+            pagar();
+
+        }
     }
 
 
-     operacion = prompt("Ingresar una operacion \n suma \n resta\n multiplicar\n dividir \n para salir escibre esc").toUpperCase();
+    function pagar() {
+
+        let pagar = prompt("desea pagar ahora? \n si \n no")
+
+        if (pagar === "si") {
+            let usuario = prompt("ingrese su usuario");
+            alert(`bienvenido ${usuario}`)
+
+            let password = prompt("ingrese su contraseña")
+
+            if (password == 1234) {
+
+                alert("procesando pago...")
+                alert("pagado con exito¡¡")
+
+            } else {
+                alert("contraseña incorrecta, cerrando ventana de pago")
+            }
+
+        } else {
+            alert(" cerrando ventana de pagos")
+        }
+    }
+}
+
+let pais = prompt("Que pais te gustaria visitar? \n Moscu - Plaza roja \n Nueva Zelanda \n Croacia \n escribe esc para salir").toLocaleLowerCase();
+
+while (pais != "esc") {
+
+    switch (pais) {
+        case "moscu":
+
+            viaje(12000);
+            break
+        case "nueva zelanda":
+
+            viaje(28990)
+            break;
+
+        case "croacia":
+            viaje(48990)
+            break;
+
+        default:
+
+            alert("opcion ingresada no es valida")
+            break;
+    }
+    pais = prompt("Que pais te gustaria visitar? \n Moscu - Plaza roja \n Nueva Zelanda \n Croacia escribe esc para salir").toLocaleLowerCase();
 
 }
