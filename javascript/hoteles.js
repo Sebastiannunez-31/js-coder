7f95fbeb36a590c4b691dc66dd2522ad7f08eac6
sebastiannunez-31 <seba.hinos@gmail.com>
2022-09-22 00:47:11 -0300

    creacion del off canvas y setear productos en el

--- javascript/hoteles.js
+++ javascript/hoteles.js
@@ -1,8 +1,12 @@
 
 
+const cards = document.getElementById('cards')
 const items = document.getElementById('items')
+const footer = document.getElementById('footerCarrito')
 const templateCard = document.getElementById('template-card').content
 const fragment = document.createDocumentFragment(); //* fragment evita reflow o que se recorra de manera innecesaria un arreglo
+const templateCanvas = document.getElementById('template-canvas').content//*template footer
+const templateCarrito = document.getElementById('template-carrito').content
 let carrito = {}
 
 
@@ -10,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchData();
 })
 
-items.addEventListener('click', e => {
+cards.addEventListener('click', e => {
 
     addCarrito(e)
 })
@@ -39,7 +43,7 @@ const pintarCards = data => {
         fragment.appendChild(clone)//* fragment evita reflow, asi no se recorre innecesariamente el arreglo
     });
 
-    items.appendChild(fragment);
+    cards.appendChild(fragment);
 
 }
 
@@ -66,15 +70,62 @@ const setCarrito = objeto => {
     }
 
     if (carrito.hasOwnProperty(producto.id)) {//*si se detecta que se agrega el mismo producto lo aumentara en uno en su cantidad total
-        producto.cantidad = carrito[producto.id].cantidad + 1
+        producto.cantidad = carrito[producto.id].cantidad + 1;
 
-    
+
     }
 
-    carrito[producto.id] = { ...producto }//*sprite operations - buscar mas informacion 
+    carrito[producto.id] = { ...producto }//*spread operations - buscar mas informacion 
     //* se accede a la informacion de carrito y se copia
 
-    console.log(carrito)
+    pintarCarrito()
 
-    alert(`has agregado ${JSON.stringify(carrito)}`)
+
+}
+
+
+const pintarCarrito = () => {
+
+    /*  console.log(carrito); */
+    items.innerHTML = ''
+    Object.values(carrito).forEach(producto => {
+        templateCarrito.querySelector('th').textContent = producto.id
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.tittle
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+        templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
+
+        const clone = templateCarrito.cloneNode(true)
+
+        fragment.appendChild(clone)
+
+       
+    })
+   
+    items.appendChild(fragment)
+    
+    pintarCanvas()
+   
+}
+
+const pintarCanvas = () => {
+
+    footer.innerHTML = ''
+
+    if (Object.keys(carrito).length === 0) {
+        footer.innerHTML = `<th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>`
+       
+    }
+
+    const totalQuantity = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
+    const totalPrice = Object.values(carrito).reduce((acc,{cantidad, precio}) => acc + cantidad * precio, 0)
+    
+    templateCanvas.querySelectorAll('td')[0].textContent=totalQuantity
+    templateCanvas.querySelector('span').textContent=totalPrice
+
+    const clone= templateCanvas.cloneNode(true)
+
+    fragment.appendChild(clone)
+    footer.appendChild(fragment)
 }
